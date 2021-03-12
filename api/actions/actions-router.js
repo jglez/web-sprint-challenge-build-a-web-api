8fe7895218d11899696c62408bcf65dce15669bf
jglez <jglez.cs@gmail.com>
2021-03-12 14:30:18 -0600

    Instantiated Actions Router and connected it with server.js

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,14 @@
 // Write your "actions" router here!
+
+// Import Express
+const express = require('express')
+
+// Import Actions Model
+const Action = require('./actions-model.js')
+
+// Instantiate Router
+const router = express.Router()
+
+/***** ACTIONS ENDPOINTS *****/
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 // Import Express
 const express = require('express');
 
-// Instantiate Server
+// Import Actions Router
+const actionsRouter = require('./actions/actions-router.js')
+
+// Instantiate Express application
 const server = express();
+
+// Enable the server to parse JSON bodies
+server.use(express.json())
+
+// Funnel Actions requests to Actions Router
+server.use('/api/actions', actionsRouter)
 
 module.exports = server;
